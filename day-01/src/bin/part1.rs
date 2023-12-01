3117ff0f2c556445680eee723a780a34b5ac906e
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-01 17:53:36 -0500

    day 1 part 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -4,8 +4,23 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(input: &str) -> i32 {
-    1
+fn part1(input: &str) -> u32 {
+    let mut count = 0;
+    for line in input.lines() {
+        let mut first: Option<u32> = None;
+        let mut second: Option<u32> = None;
+        for c in line.chars() {
+            if let Some(as_digit) = c.to_digit(10) {
+                if first.is_none() {
+                    first = Some(as_digit);
+                }
+                second = Some(as_digit);
+            }
+        }
+        let line_result = first.unwrap() * 10 + second.unwrap();
+        count += line_result;
+    }
+    count
 }
 
 #[cfg(test)]
@@ -13,12 +28,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn part1_test() {
         let result = part1("1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet");
-        assert_eq!(result, 3);
+        assert_eq!(result, 142);
     }
 }
 
