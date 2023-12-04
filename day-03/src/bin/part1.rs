89ef72a2c5ee8b684e229da850ad517d14f6808a
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-04 04:28:58 -0500

    day 3 part 1

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -5,7 +5,55 @@ fn main() {
 }
 
 fn part1(input: &str) -> u32 {
-    todo!();
+    let input_lines: Vec<&str> = input.lines().collect();
+    input_lines
+        .iter()
+        .enumerate()
+        .map(|(i, &line)| {
+            let mut chars = line.chars().enumerate();
+            let mut line_sum = 0;
+            loop {
+                let number_character_map = chars
+                    .by_ref()
+                    .skip_while(|(_, c)| !c.is_numeric())
+                    .take_while(|(_, c)| c.is_numeric())
+                    .map(|(j, c)| {
+                        for dy in -1..=1 {
+                            for dx in -1..=1 {
+                                if i as i32 + dy < 0 {
+                                    continue;
+                                }
+                                match input_lines.get((i as i32 + dy) as usize) {
+                                    Some(&line_check) => {
+                                        if j as i32 + dx < 0 {
+                                            continue;
+                                        }
+                                        match line_check.chars().nth((j as i32 + dx) as usize) {
+                                            Some(c_check) => {
+                                                if !c_check.is_numeric() && c_check != '.' {
+                                                    return (c, true);
+                                                }
+                                            }
+                                            None => (),
+                                        }
+                                    }
+                                    None => (),
+                                }
+                            }
+                        }
+                        (c, false)
+                    })
+                    .collect::<Vec<(char, bool)>>();
+                if number_character_map.len() == 0 {
+                    break;
+                }
+                if number_character_map.iter().any(|(_, b)| *b) {
+                    line_sum += number_character_map.iter().map(|(c, _)| *c).collect::<String>().parse::<u32>().unwrap();
+                }
+            }
+            line_sum
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -16,15 +64,15 @@ mod tests {
     fn part1_test() {
         let result = part1(
             "467..114..
-            ...*......
-            ..35..633.
-            ......#...
-            617*......
-            .....+.58.
-            ..592.....
-            ......755.
-            ...$.*....
-            .664.598..",
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..",
         );
         assert_eq!(result, 4361);
     }
