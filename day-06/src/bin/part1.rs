0f800019853165e6932c8e9dcc72aa84df0b1a18
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-06 01:22:41 -0500

    day 6 rust setup

---
+++ day-06/src/bin/part1.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> u32 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let result = part1(
+            "Time:      7  15   30
+Distance:  9  40  200",
+        );
+        assert_eq!(result, 288);
+    }
+}
