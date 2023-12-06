dd5283373433e92084e68f05148fc0f7f994ea7c
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-05 19:52:07 -0500

    day 5 part 2

--- day-05/src/bin/part2.rs
+++ day-05/src/bin/part2.rs
@@ -1,18 +1,18 @@
 fn main() {
     let input = include_str!("./input.txt");
-    let output = part1(input);
+    let output = part2(input);
     dbg!(output);
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Range {
+struct Interval {
     start: usize,
     end: usize,
 }
 
-impl Range {
-    fn new(start: usize, end: usize) -> Range {
-        Range { start, end }
+impl Interval {
+    fn new(start: usize, end: usize) -> Interval {
+        Interval { start, end }
     }
 
     fn start(&self) -> usize {
@@ -36,23 +36,26 @@ impl Range {
     }
 }
 
-fn part1(input: &str) -> usize {
+fn part2(input: &str) -> usize {
     let mut input_lines = input.lines();
-    let seeds: Vec<usize> = input_lines
+    let binding = input_lines
         .next()
         .unwrap()
         .chars()
         .skip_while(|c| *c != ':')
         .skip(1)
-        .collect::<String>()
-        .split_whitespace()
-        .map(|number| number.parse().unwrap())
-        .collect();
+        .collect::<String>();
+    let binding = binding.split_whitespace().collect::<Vec<&str>>();
+    let seeds = binding.chunks(2).flat_map(|pair| {
+        pair[0].parse::<usize>().unwrap()
+            ..pair[1].parse::<usize>().unwrap() + pair[0].parse::<usize>().unwrap()
+    });
+    // .map(|number| number.parse().unwrap())
     let _ = input_lines.by_ref().next();
-    let mut mappings: Vec<Vec<[Range; 2]>> = Vec::new();
+    let mut mappings: Vec<Vec<[Interval; 2]>> = Vec::new();
     loop {
         let _ = input_lines.by_ref().next();
-        let mut mapping: Vec<[Range; 2]> = input_lines
+        let mut mapping: Vec<[Interval; 2]> = input_lines
             .by_ref()
             .take_while(|&line| line != "")
             .map(|line| {
@@ -61,8 +64,8 @@ fn part1(input: &str) -> usize {
                     .map(|number| number.parse::<usize>().unwrap())
                     .collect();
                 [
-                    Range::new(line_numbers[1], line_numbers[1] + line_numbers[2]),
-                    Range::new(line_numbers[0], line_numbers[0] + line_numbers[2]),
+                    Interval::new(line_numbers[1], line_numbers[1] + line_numbers[2]),
+                    Interval::new(line_numbers[0], line_numbers[0] + line_numbers[2]),
                 ]
             })
             .collect();
@@ -79,8 +82,8 @@ fn part1(input: &str) -> usize {
                 mapping.insert(
                     i,
                     [
-                        Range::new(prev_end, curr_start),
-                        Range::new(prev_end, curr_start),
+                        Interval::new(prev_end, curr_start),
+                        Interval::new(prev_end, curr_start),
                     ],
                 );
                 i += 1;
@@ -89,20 +92,21 @@ fn part1(input: &str) -> usize {
             i += 1;
         }
         mapping.push([
-            Range::new(prev_end, usize::max_value()),
-            Range::new(prev_end, usize::max_value()),
+            Interval::new(prev_end, usize::max_value()),
+            Interval::new(prev_end, usize::max_value()),
         ]);
         mapping.sort_by(|r, r2| r[0].start().cmp(&r2[0].start()));
         mappings.push(mapping);
     }
+    let mut count: u64 = 0;
     seeds
-        .iter()
-        .map(|&seed| {
+        .map(|seed| {
+            count += 1;
+            if count % 10000000 == 0 {
+                dbg!(count);
+            }
             mappings.iter().fold(seed, |source, mapping| {
-                let source_map = mapping
-                    .iter()
-                    .find(|&map| map[0].contains(source))
-                    .unwrap();
+                let source_map = mapping.iter().find(|&map| map[0].contains(source)).unwrap();
                 source_map[1].nth(source_map[0].get_nth(source))
             })
         })
@@ -115,8 +119,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_test() {
-        let result = part1(
+    fn part2_test() {
+        let result = part2(
             "seeds: 79 14 55 13
 
 seed-to-soil map:
@@ -151,6 +155,6 @@ humidity-to-location map:
 60 56 37
 56 93 4",
         );
-        assert_eq!(result, 35);
+        assert_eq!(result, 46);
     }
 }
