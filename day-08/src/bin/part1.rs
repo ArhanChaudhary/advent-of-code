65b09ae52972fe344b618fbef3fc40f369e0743c
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-08 00:00:13 -0500

    day 8 rust setup

---
+++ day-08/src/bin/part1.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let result = part1(
+            "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)",
+        );
+        assert_eq!(result, 6);
+    }
+}
