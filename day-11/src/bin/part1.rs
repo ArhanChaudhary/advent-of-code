ba1a101cde5b01ded9b6be04b5351866a32b2847
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-11 00:00:13 -0500

    day 11 rust setup

---
+++ day-11/src/bin/part1.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let result = part1(
+            "....1........
+.........2...
+3............
+.............
+.............
+........4....
+.5...........
+.##.........6
+..##.........
+...##........
+....##...7...
+8....9.......",
+        );
+        assert_eq!(result, 374);
+    }
+}
