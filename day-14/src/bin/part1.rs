d292651e57450faf459b1ab74bfe4ab70b45ae36
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-14 23:00:39 -0800

    day 14 part 1

--- day-14/src/bin/part1.rs
+++ day-14/src/bin/part1.rs
@@ -5,7 +5,26 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    todo!();
+    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let row_count = grid.len();
+    let col_count = grid[0].len();
+    for j in 0..col_count {
+        for i in 0..row_count {
+            if grid[i][j] != 'O' {
+                continue;
+            }
+            let mut k = i;
+            while k >= 1 && grid[k - 1][j] == '.' {
+                grid[k - 1][j] = 'O';
+                grid[k][j] = '.';
+                k -= 1;
+            }
+        }
+    }
+    grid.into_iter()
+        .enumerate()
+        .map(|(i, line)| (row_count - i) * line.into_iter().filter(|&c| c == 'O').count())
+        .sum()
 }
 
 #[cfg(test)]
