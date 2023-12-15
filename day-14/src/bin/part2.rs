bf4f3e77ba576519c3201dec0ed9e1e3876bfed3
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-15 01:32:28 -0800

    day 14 part 2

---
+++ day-14/src/bin/part2.rs
@@ -0,0 +1,110 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn spin_cycle(grid: &mut Grid) {
+    let row_count = grid.len();
+    let col_count = grid[0].len();
+    for j in 0..col_count {
+        for i in 0..row_count {
+            if grid[i][j] != 'O' {
+                continue;
+            }
+            let mut k = i;
+            while k as isize - 1 >= 0 && grid[k - 1][j] == '.' {
+                grid[k - 1][j] = 'O';
+                grid[k][j] = '.';
+                k -= 1;
+            }
+        }
+    }
+
+    for i in 0..row_count {
+        for j in 0..col_count {
+            if grid[i][j] != 'O' {
+                continue;
+            }
+            let mut k = j;
+            while k as isize - 1 >= 0 && grid[i][k - 1] == '.' {
+                grid[i][k - 1] = 'O';
+                grid[i][k] = '.';
+                k -= 1;
+            }
+        }
+    }
+
+    for j in 0..col_count {
+        for i in (0..row_count).rev() {
+            if grid[i][j] != 'O' {
+                continue;
+            }
+            let mut k = i;
+            while k + 1 < row_count && grid[k + 1][j] == '.' {
+                grid[k + 1][j] = 'O';
+                grid[k][j] = '.';
+                k += 1;
+            }
+        }
+    }
+
+    for i in 0..row_count {
+        for j in (0..col_count).rev() {
+            if grid[i][j] != 'O' {
+                continue;
+            }
+            let mut k = j;
+            while k + 1 < col_count && grid[i][k + 1] == '.' {
+                grid[i][k + 1] = 'O';
+                grid[i][k] = '.';
+                k += 1;
+            }
+        }
+    }
+}
+
+type Grid = Vec<Vec<char>>;
+fn part1(input: &str) -> usize {
+    let mut grid: Grid = input.lines().map(|line| line.chars().collect()).collect();
+    let mut seen_grids: Vec<Grid> = Vec::new();
+    loop {
+        if let Some(duplicate_grid_index) =
+            seen_grids.iter().position(|seen_grid| seen_grid == &grid)
+        {
+            let final_grid = &seen_grids[duplicate_grid_index
+                + (1000000000 - duplicate_grid_index) % (seen_grids.len() - duplicate_grid_index)];
+            let row_count = final_grid.len();
+            break final_grid
+                .iter()
+                .enumerate()
+                .map(|(i, line)| (row_count - i) * line.iter().filter(|&&c| c == 'O').count())
+                .sum();
+        }
+        seen_grids.push(grid.clone());
+        spin_cycle(&mut grid);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn part1_test() {
+        let result = part1("\
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....",
+        );
+        assert_eq!(result, 64);
+    }
+}
