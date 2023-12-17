76fab67e9bead866d9b1f4fb0fce51ae6f1b8738
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-17 01:07:17 -0800

    day 17 rust setup

---
+++ day-17/src/bin/part1.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn part1_test() {
+        let result = part1("\
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533",
+        );
+        assert_eq!(result, 102);
+    }
+}
