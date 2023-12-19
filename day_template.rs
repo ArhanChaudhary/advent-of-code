4cd45072f66a282c76862136e92d0393ccbbe08f
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-18 21:50:56 -0800

    switch day_template function order

--- day_template.rs
+++ day_template.rs
@@ -1,11 +1,11 @@
+fn part1(input: &str) -> usize {
+    todo!();
+}
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part1(input);
     dbg!(output);
-}
-
-fn part1(input: &str) -> usize {
-    todo!();
 }
 
 #[cfg(test)]
