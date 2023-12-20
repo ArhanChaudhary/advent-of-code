e97bb78010952356478ecb9e5de6c1447e270579
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-20 14:18:27 -0800

    day 19 part 1

--- day-19/src/bin/part1.rs
+++ day-19/src/bin/part1.rs
@@ -1,5 +1,197 @@
+use itertools::FoldWhile::{Continue, Done};
+use itertools::Itertools;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct Workflow<'a> {
+    name: String,
+    rules: RefCell<Vec<Rule<'a>>>,
+}
+
+impl<'a> Workflow<'a> {
+    fn add_rule(&self, rule: Rule<'a>) {
+        self.rules.borrow_mut().insert(0, rule);
+    }
+
+    fn to_dest(&self, part: &Part) -> Destination<'a> {
+        self.rules
+            .borrow()
+            .iter()
+            .find_map(|rule| {
+                if rule.matches(part) {
+                    Some(rule.destination)
+                } else {
+                    None
+                }
+            })
+            .unwrap()
+    }
+}
+
+#[derive(Debug)]
+struct Rule<'a> {
+    condition: Option<Condition>,
+    destination: Destination<'a>,
+}
+
+impl Rule<'_> {
+    fn matches(&self, part: &Part) -> bool {
+        match &self.condition {
+            None => true,
+            Some(condition) => {
+                let operand1 = match condition.operand1 {
+                    PartRating::X => part.x,
+                    PartRating::M => part.m,
+                    PartRating::A => part.a,
+                    PartRating::S => part.s,
+                };
+                match condition.operator {
+                    '>' => operand1 > condition.operand2,
+                    '<' => operand1 < condition.operand2,
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Destination<'a> {
+    R,
+    A,
+    Workflow(&'a Workflow<'a>),
+}
+
+impl Destination<'_> {
+    fn from<'a>(raw_from: &str, workflows: &'a [Workflow<'a>]) -> Destination<'a> {
+        match raw_from {
+            "A" => Destination::A,
+            "R" => Destination::R,
+            workflow_name => Destination::Workflow(
+                workflows
+                    .iter()
+                    .find(|workflow| workflow.name == workflow_name)
+                    .unwrap(),
+            ),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Condition {
+    operator: char,
+    operand1: PartRating,
+    operand2: usize,
+}
+
+#[derive(Debug)]
+enum PartRating {
+    X,
+    M,
+    A,
+    S,
+}
+
+#[derive(Debug)]
+struct Part {
+    x: usize,
+    m: usize,
+    a: usize,
+    s: usize,
+}
+
+impl Part {
+    fn rating_sum(&self) -> usize {
+        self.x + self.m + self.a + self.s
+    }
+}
+
 fn part1(input: &str) -> usize {
-    todo!();
+    let mut input_split = input.split("\n\n");
+    let mut workflows: Vec<Workflow> = Vec::new();
+    let mut raw_rules: Vec<String> = Vec::new();
+    for line in input_split.next().unwrap().lines() {
+        let mut line_chars = line.chars();
+        let workflow = Workflow {
+            name: line_chars.by_ref().take_while(|&c| c != '{').collect(),
+            rules: RefCell::from(Vec::new()),
+        };
+        let raw_rule = line_chars.take_while(|&c| c != '}').collect();
+        workflows.push(workflow);
+        raw_rules.push(raw_rule);
+    }
+    let root_workflow = workflows
+        .iter()
+        .find(|workflow| workflow.name == "in")
+        .unwrap();
+    for (workflow, raw_rules) in workflows.iter().zip(raw_rules) {
+        // workflow.rules
+        let mut raw_rules_split = raw_rules.rsplit(',');
+        workflow.add_rule(Rule {
+            condition: None,
+            destination: Destination::from(raw_rules_split.next().unwrap(), &workflows),
+        });
+        for raw_rule in raw_rules_split {
+            let mut raw_rule_chars = raw_rule.chars();
+            let operand1 = match raw_rule_chars.next() {
+                Some('x') => PartRating::X,
+                Some('m') => PartRating::M,
+                Some('a') => PartRating::A,
+                Some('s') => PartRating::S,
+                _ => unreachable!(),
+            };
+            let operator = raw_rule_chars.next().unwrap();
+            let operand2 = raw_rule_chars
+                .by_ref()
+                .take_while(|&c| c != ':')
+                .fold(0, |sum, c| sum * 10 + c.to_digit(10).unwrap() as usize);
+            let raw_from = raw_rule_chars.collect::<String>();
+            let raw_from = raw_from.as_str();
+            workflow.add_rule(Rule {
+                condition: Some(Condition {
+                    operator,
+                    operand1,
+                    operand2,
+                }),
+                destination: Destination::from(raw_from, &workflows),
+            })
+        }
+    }
+
+    input_split
+        .next()
+        .unwrap()
+        .lines()
+        .filter_map(|raw_part_ratings| {
+            let mut raw_part_ratings_chars = raw_part_ratings.chars();
+            let mut next_part_rating = || {
+                raw_part_ratings_chars
+                    .by_ref()
+                    .skip_while(|&c| !c.is_numeric())
+                    .fold_while(0, |num, c| match c.to_digit(10) {
+                        Some(n) => Continue(num * 10 + n as usize),
+                        None => Done(num),
+                    })
+                    .into_inner()
+            };
+            let part = Part {
+                x: next_part_rating(),
+                m: next_part_rating(),
+                a: next_part_rating(),
+                s: next_part_rating(),
+            };
+            let mut current_workflow = root_workflow;
+            loop {
+                match current_workflow.to_dest(&part) {
+                    Destination::Workflow(dest_workflow) => {
+                        current_workflow = dest_workflow;
+                    }
+                    Destination::A => break Some(part.rating_sum()),
+                    Destination::R => break None,
+                }
+            }
+        })
+        .sum()
 }
 
 fn main() {
