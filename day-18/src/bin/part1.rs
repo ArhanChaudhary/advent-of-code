02cc56dd0ad8269dc1764c4537f4589e0f98ba47
Arhan Chaudhary <arhan.ch@gmail.com>
2023-12-20 02:44:40 -0800

    day 18 part 1

--- day-18/src/bin/part1.rs
+++ day-18/src/bin/part1.rs
@@ -1,5 +1,61 @@
+use itertools::Itertools;
+
+#[derive(Clone, Copy)]
+struct Point(isize, isize);
+
 fn part1(input: &str) -> usize {
-    todo!();
+    let mut current_point = Point(0, 0);
+    let mut border_displacement = 0;
+    let mut boundary: Vec<Point> = Vec::new();
+    for (line_direction, line_displacement) in input.lines().map(|line| {
+        let mut split = line.split_whitespace();
+        (
+            split.next().unwrap().chars().exactly_one().unwrap(),
+            split.next().unwrap().parse::<usize>().unwrap(),
+        )
+    }) {
+        border_displacement += line_displacement;
+        match line_direction {
+            'U' => {
+                current_point.1 += line_displacement as isize;
+            }
+            'R' => {
+                current_point.0 += line_displacement as isize;
+            }
+            'D' => {
+                current_point.1 -= line_displacement as isize;
+            }
+            'L' => {
+                current_point.0 -= line_displacement as isize;
+            }
+            _ => unreachable!(),
+        }
+        boundary.push(current_point);
+    }
+    let area = (0..boundary.len())
+        .map(|i| {
+            let curr_y = boundary[i].1;
+            let prev_x = if i == 0 {
+                boundary[boundary.len() - 1]
+            } else {
+                boundary[i - 1]
+            }
+            .0;
+            let next_x = if i == boundary.len() - 1 {
+                boundary[0]
+            } else {
+                boundary[i + 1]
+            }
+            .0;
+            curr_y * (prev_x - next_x)
+        })
+        .sum::<isize>()
+        .abs() as usize
+        / 2;
+    // A = i + b/2 - 1
+    // i = A - b/2 + 1
+    let interior_points = area - border_displacement / 2 + 1;
+    interior_points + border_displacement
 }
 
 fn main() {
