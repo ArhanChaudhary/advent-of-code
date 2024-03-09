eeca54e2db619c17c1cf58043c8ea12132497973
Arhan Chaudhary <arhan.ch@gmail.com>
2024-03-09 14:24:09 -0800

    day 24 rust setup

---
+++ day-24/src/bin/part1.rs
@@ -0,0 +1,27 @@
+fn part1(input: &str) -> usize {
+    todo!();
+}
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn part1_test() {
+        let result = part1("\
+19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3",
+        );
+        assert_eq!(result, 2);
+    }
+}
